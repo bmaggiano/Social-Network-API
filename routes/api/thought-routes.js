df78861de1e529b21f83ad498b7acb357f6b92ef
bmaggiano <brandon.maggiano@gmail.com>
2022-11-07 18:34:12 -0700

    initial api routes, and controller folder set up

---
+++ routes/api/thought-routes.js
@@ -0,0 +1,15 @@
+const router = require('express').Router()
+
+//need to require functions from controller
+
+//need a get route to get all thoughts
+
+//need a get route to get a single throught by it's _id
+
+//need a post route to create a new thought
+
+//need a put route to update a thought by it's _id
+
+//need a delete route to remove through by it's _id
+
+//need /api/thoughts/:thoughtId/reactions post, and delete route
---
+++ routes/api/user-routes.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+
+//need to require all functions from controllers
+
+//need a get route for all users
+
+//need a get route for a single user by it's _id and populated thought and friend data
+
+//need a post route for a new user
+
+module.exports = router;
